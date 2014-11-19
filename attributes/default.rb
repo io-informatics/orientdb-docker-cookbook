bff1a6bb7c2b65659fd89c6b396797dd54f50513
Alexander De Leon <me@alexdeleon.name>
2014-11-19 16:30:26 +0100

    Added basic configuration for server config file

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,8 @@ default[:orientdb][:docker_container]="orientdb"
 default[:orientdb][:config_path]="/etc/orientdb"
 default[:orientdb][:databases_path]="/var/data/orientdb/databases"
 default[:orientdb][:backup_path]="/var/backup/orientdb"
+
+default[:orientdb][:server]['db.pool.min'] = 1
+default[:orientdb][:server]['db.pool.max'] = 50
+default[:orientdb][:server]['cache.local.enabled'] = true
+default[:orientdb][:server]['profiler.enabled'] = true
